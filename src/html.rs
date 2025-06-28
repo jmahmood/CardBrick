e13fcbd29bf5bec93a01fc095de2d3f6dd16c26b
Jawaad Mahmood <ideas@jawaadmahmood.com>
2025-06-27 21:07:41 -0600

    Fix HTML parsing helper

--- src/html.rs
+++ src/html.rs
@@ -8,26 +8,30 @@ pub fn html_to_text(html: &str) -> String {
 
     let parser = dom.parser();
     let mut out = String::new();
-    for child in dom.children_of(dom.document()) {
-        extract_text(child, &dom, parser, &mut out);
+
+    // Walk the first node in the document and gather text
+    if let Some(root) = dom.nodes().iter().next().cloned() {
+        extract_text(root, &dom, parser, &mut out);
     }
+
     out
 }
 
 fn extract_text(handle: tl::NodeHandle, dom: &tl::VDom, parser: &tl::Parser, out: &mut String) {
-    let node = &dom[handle];
-    match node {
-        tl::Node::Tag(tag) => {
-            for child in tag.children() {
-                extract_text(*child, dom, parser, out);
-            }
-            if let Some(name) = tag.name().as_utf8_str() {
-                if name == "br" || name == "p" {
-                    out.push('\n');
+    if let Some(node) = dom.get(handle) {
+        match node {
+            tl::Node::Tag(tag) => {
+                for child in tag.children().iter() {
+                    extract_text(*child, dom, parser, out);
+                }
+                if let Some(name) = tag.name().as_ref().and_then(|n| n.as_utf8_str()) {
+                    if name == "br" || name == "p" {
+                        out.push('\n');
+                    }
                 }
             }
+            tl::Node::Raw(text) => out.push_str(text.as_utf8_str()),
+            _ => {}
         }
-        tl::Node::Text(text) => out.push_str(text.as_utf8_str()),
-        _ => {}
     }
 }
